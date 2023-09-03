797dcbb0555a718d6c2a28d0e59ce1d3a9583aac
afolabi mabunmi <brownboy3047@gmail.com>
2023-09-03 09:33:58 +0100

    Refactor some code in consulting and caseStudy folders

--- src/pages/landingPage/consulting/Consulting.jsx
+++ src/pages/landingPage/consulting/Consulting.jsx
@@ -5,21 +5,38 @@ import line from "../../../assets/line.png";
 
 //style
 import "./Consulting.css";
+import CaseStudyCard from "../case-study/CaseStudyCard";
 
 const Consulting = () => {
   return (
-    <div className="consulting">
-      <ConsultingCard />
-      <img className="image" src={line} alt="line" />
+    <div className="consulting-container">
+      <div className="consulting">
+        <ConsultingCard />
+        <img className="image" src={line} alt="line" />
 
-      <ConsultingCard />
-      <img className="image" src={line} alt="line" />
+        <ConsultingCard />
+        <img className="image" src={line} alt="line" />
 
-      <ConsultingCard />
-      <img className="image" src={line} alt="line" />
+        <ConsultingCard />
+        <img className="image" src={line} alt="line" />
 
-      <ConsultingCard />
-      <img className="image" src={line} alt="line" />
+        <ConsultingCard />
+        <img className="image" src={line} alt="line" />
+      </div>
+
+      <div className="consulting-growth">
+        <CaseStudyCard className="consulting-growth-card">
+          BUSINESS GROWTH
+        </CaseStudyCard>
+
+        <CaseStudyCard className="consulting-growth-card">
+          BUSINESS GROWTH
+        </CaseStudyCard>
+
+        <CaseStudyCard className="consulting-growth-card">
+          BUSINESS GROWTH
+        </CaseStudyCard>
+      </div>
     </div>
   );
 };
